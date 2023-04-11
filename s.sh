207f2c6195bac91b13b2359bfc6f565a299b36b6
Adhithya Pasupuleti <92710784+Adhithya8055@users.noreply.github.com>
2023-04-11 17:42:00 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -8,33 +8,20 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Navigate to the directory where the script is located
 cd "$SCRIPT_DIR"
 
-# Show the ASCII and logo stuff here
+# Check for updates
+git fetch
 
-while true; do
-  # Check for updates
-  git fetch > /dev/null 2>&1
+# Compare the local branch with the remote branch
+if [ "$(git rev-list --count --left-right HEAD...@{upstream} 2>/dev/null | awk '{print $2}')" != "0" ]; then
+  # Update the local repository
+  git pull
 
-  # Compare the local branch with the remote branch
-  if [ "$(git rev-list --count --left-right HEAD...@{upstream} 2>/dev/null | awk '{print $2}')" != "0" ]; then
-    # Update the local repository
-    echo "An update is available. Do you want to update? [y/n]"
-    read -r answer
-    if [[ "$answer" == [yY] ]]; then
-      git pull > /dev/null 2>&1
-      echo "Airspire has been updated. Relaunching..."
-      exec bash "$0"
-      exit 0
-    fi
-  else
-    # If no update is available, continue with the existing script
-    echo "Airspire is up to date. Continuing..."
-  fi
-
+  # Launch the updated script
+  echo "Launching updated airspire..."
+  exec bash "$SCRIPT_DIR/airspire.sh"
+else
   # Add your existing code here
-  
-  # Sleep for 5 seconds before checking for updates again
-  sleep 5
-  
+  echo "Airspire is up to date. Continuing..."
   echo "test"
-#echo "next"
-done
+fi
+
