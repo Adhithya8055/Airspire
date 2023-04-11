679c2b4e7d19e27e261010cd17af68619dca3512
Adhithya Pasupuleti <92710784+Adhithya8055@users.noreply.github.com>
2023-04-11 17:32:04 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -8,20 +8,24 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Navigate to the directory where the script is located
 cd "$SCRIPT_DIR"
 
-# Check for updates
-git fetch
+while true; do
+  # Check for updates
+  git fetch
 
-# Compare the local branch with the remote branch
-if [ "$(git rev-list --count --left-right HEAD...@{upstream} 2>/dev/null | awk '{print $2}')" != "0" ]; then
-  # Update the local repository
-  git pull
+  # Compare the local branch with the remote branch
+  if [ "$(git rev-list --count --left-right HEAD...@{upstream} 2>/dev/null | awk '{print $2}')" != "0" ]; then
+    # Update the local repository
+    git pull
 
-  # Launch the updated script
-  echo "Launching updated airspire..."
-  exec bash "$SCRIPT_DIR/airspire.sh"
-else
+    # Launch the updated script
+    echo "Launching updated airspire..."
+    exec bash "$SCRIPT_DIR/airspire.sh"
+  fi
+  
   # Add your existing code here
   echo "ok"
   echo "Ayo"
-fi
-
+  
+  # Sleep for 5 seconds before checking for updates again
+  sleep 5
+done
