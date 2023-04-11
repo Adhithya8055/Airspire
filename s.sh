177d67c1b26c08be1500fd0fbd77cd0aebe319f3
Adhithya Pasupuleti <92710784+Adhithya8055@users.noreply.github.com>
2023-04-11 13:16:20 +0530

    Update and rename dev to s.sh

---
+++ s.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add the Git repository URL as a remote called "origin"
+#git remote add origin https://github.com/Adhithya8055/Airspire.git
+
+# Pull changes from the remote "origin"
+#git pull origin main
+git pull origin main >/dev/null 2>&1
+
+
+# Run the rest of your installation script here...
+
+
+echo "dome"
+echo "null"
