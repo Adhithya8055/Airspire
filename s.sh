2a51954cf95a6b8704a5b831fd3840d922ca6ac8
Adhithya Pasupuleti <92710784+Adhithya8055@users.noreply.github.com>
2023-04-11 17:16:03 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -2,30 +2,34 @@
 
 echo "Launching airspire..."
 
+# Get the absolute path of the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Navigate to the directory where the script is located
-cd /path/to/airspire
+cd "$SCRIPT_DIR"
 
-# Check for updates
-git fetch
+# Check if the current directory is a Git repository
+if [ -d ".git" ]; then
+  # Check for updates
+  git fetch
 
-# Compare the local branch with the remote branch
-if [ "$(git rev-list HEAD...origin/master --count)" -gt 0 ]; then
-  # Update the local repository
-  git pull
+  # Compare the local branch with the remote branch
+  if [ "$(git rev-list HEAD...origin/master --count)" -gt 0 ]; then
+    # Update the local repository
+    git pull
 
-  # Launch the updated script
-  echo "Launching updated airspire..."
-  bash airspire.sh
+    # Launch the updated script
+    echo "Launching updated airspire..."
+    bash "$SCRIPT_DIR/airspire.sh"
 
-  # Exit the current instance of the script
-  exit 0
+    # Exit the current instance of the script
+    exit 0
+  fi
+else
+  echo "ERROR: The current directory is not a Git repository."
+  exit 1
 fi
 
-# If no update is available, continue with the existing script
+# If no update is available or the script is not in a Git repository, continue with the existing script
 echo "No update available. Continuing with existing airspire..."
 # Add your existing code here
-
-
-echo "this is a test"
-
-echo "N5"
